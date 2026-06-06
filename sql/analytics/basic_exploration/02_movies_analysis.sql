1bbe223746b93d0acd44b9e0fd27d10398b82301
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-06-06 19:41:25 -0300

    exploracion basica de las tablas completada

--- sql/analytics/basic_exploration/02_movies_analysis.sql
+++ sql/analytics/basic_exploration/02_movies_analysis.sql
@@ -1,1 +1,19 @@
+-- Película mas larga
+SELECT title, duration_minutes
+FROM movies
+ORDER BY duration_minutes DESC
+LIMIT 1;
+
+-- Película mas corta
+SELECT title, duration_minutes
+FROM movies
+ORDER BY duration_minutes ASC
+LIMIT 1;
+
+SELECT
+  main_genre,
+  COUNT(movie_id) as amount_movies
+FROM movies
+GROUP BY main_genre
+ORDER BY amount_movies DESC;
 
--- sql/analytics/basic_exploration/03_event_analysis.sql
+++ sql/analytics/basic_exploration/03_event_analysis.sql
@@ -1,3 +1,25 @@
 -- Watch time promedio
 SELECT AVG(watch_time_minutes) as watchtime_promedio
 FROM viewing_session;
+
+-- Porcentaje de sesiones completadas
+SELECT
+  ROUND(AVG(completed) * 100, 2) AS porcentaje
+FROM viewing_session;
+
+-- Sesiones por idioma
+SELECT
+  language,
+  COUNT(movie_id) as cantidad
+FROM viewing_session
+GROUP BY language
+ORDER BY cantidad DESC;
+
+
+-- Sesiones por calidad
+SELECT
+  video_quality,
+  COUNT(movie_id) as cantidad
+FROM viewing_session
+GROUP BY video_quality
+ORDER BY cantidad DESC;
