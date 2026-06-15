dbe1133eb5946cab075ac746f848d578ee446f81
tachyon-lhc <valentingonzalezdaumes@gmail.com>
2026-06-14 22:26:38 -0300

    nuevo ejercicio

--- sql/analytics/05_window_functions/01_window_functions.sql
+++ sql/analytics/05_window_functions/01_window_functions.sql
@@ -5,6 +5,83 @@ SELECT
   ROW_NUMBER() OVER (
     PARTITION BY user_id
     ORDER BY watch_time_minutes DESC
-  ) as rn
+  ) AS rn
 FROM viewing_session
 LIMIT 10;
+
+-- Ranking películas por cantidad de visualizaciones
+WITH base AS (
+  SELECT
+    movie_id,
+    COUNT(*) AS cantidad_visualizaciones
+  FROM viewing_session
+  GROUP BY movie_id
+)
+SELECT
+  movie_id,
+  cantidad_visualizaciones,
+  RANK() OVER (
+    ORDER BY cantidad_visualizaciones DESC
+  ) AS rn
+FROM base
+LIMIT 10;
+
+-- Top películas por genero (ROW_NUMBER)
+WITH base AS (
+  SELECT
+    m.movie_id,
+    m.main_genre,
+    COUNT(*) AS cantidad_visualizaciones
+  FROM viewing_session v
+  JOIN movies m ON v.movie_id = m.movie_id
+  GROUP BY m.movie_id
+),
+top AS (
+  SELECT
+    main_genre,
+    movie_id,
+    cantidad_visualizaciones,
+    ROW_NUMBER() OVER (
+      PARTITION BY main_genre
+      ORDER BY cantidad_visualizaciones DESC
+    ) AS rn
+  FROM base
+)
+SELECT
+  main_genre,
+  movie_id,
+  cantidad_visualizaciones,
+  rn
+FROM top
+WHERE rn <= 5;
+
+-- Segunda película mas vista por genero (RANK)
+WITH base AS (
+  SELECT
+    m.movie_id,
+    m.main_genre,
+    COUNT(*) AS cantidad_visualizaciones
+  FROM viewing_session v
+  JOIN movies m ON v.movie_id = m.movie_id
+  GROUP BY m.movie_id
+),
+top AS (
+  SELECT
+    main_genre,
+    movie_id,
+    cantidad_visualizaciones,
+    RANK() OVER (
+      PARTITION BY main_genre
+      ORDER BY cantidad_visualizaciones DESC
+    ) AS rnk
+  FROM base
+)
+SELECT
+  main_genre,
+  movie_id,
+  cantidad_visualizaciones,
+  rnk
+FROM top
+WHERE rnk = 2;
+
+
